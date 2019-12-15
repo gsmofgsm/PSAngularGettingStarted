1858d4343997bfb849b30888c19e614cd937400f
Qing Ma <qing.ma@isatis-health.nl>
2019-12-15 08:36:56 +0800

    move shared logic from ProductModule to SharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StarComponent } from './star.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    StarComponent,
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    StarComponent,
+    CommonModule,
+    FormsModule
   ]
 })
 export class SharedModule { }
